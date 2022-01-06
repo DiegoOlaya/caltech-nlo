628fc6c2de8606ebad714ede019ebcbaa3822a47
Diego Olaya <dolaya@caltech.edu>
2022-01-05 16:15:36 -0800

    Add method stubs.

--- RotaryStage.py
+++ RotaryStage.py
@@ -9,6 +9,22 @@ class RotaryStage:
             id: The VISA id string for the powermeter.
         '''
         self.stage = rm.open_resource(address)
-        self.stepPerRev = 262144
+        self.stepPerDeg = 262144/360 #Conversion constant.
         self.stage.read_termination = '\r\n'
         self.stage.write('0ho0') #Home the motor.
+
+    def move_to_pos(self, pos):
+        '''Moves the motor to a set absolute position in degrees. \n
+        Inputs:
+            pos: The position in degrees to move the stage to.
+        '''
+        pass
+
+    def get_pos(self):
+        '''Returns current position, in degrees, of the motor.
+        '''
+        pass
+
+    def close(self):
+        '''Closes VISA connection.'''
+        self.stage.close()
