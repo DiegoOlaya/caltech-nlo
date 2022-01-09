b4b9bb4f5c3768c62336eee40b9b638d8720311e
Diego Olaya <dolaya@caltech.edu>
2022-01-09 12:58:41 -0800

    Implement move function.

--- RotaryStage.py
+++ RotaryStage.py
@@ -18,7 +18,9 @@ class RotaryStage:
         Inputs:
             pos: The position in degrees to move the stage to.
         '''
-        pass
+        pos_in_steps = self.stepPerDeg * pos
+        hex_pos = "{0:0{1}x}".format(pos_in_steps, 8) #Converts decimal position to 8-digit hexadecimal.
+        self.stage.write("0ma" + hex_pos)
 
     def get_pos(self):
         '''Returns current position, in degrees, of the motor.
